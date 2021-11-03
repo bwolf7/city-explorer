ae6c42d9c6e34e0f6e86a45400114918b52019c5
Brandon <brandon.wolf1@outlook.com>
2021-11-02 19:01:34 -0700

    Removed city card

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,14 @@
 import { Component } from "react";
 import FormLocation from "./FormLocation.js"
 import DisplayModal from "./DisplayModal.js";
-import Citycard from "./Citycard.js";
+//import Citycard from "./Citycard.js";
 
 class Main extends Component {
     render() {
         return (
             <>
                 <FormLocation handleClick={this.props.handleClick} handleChange={this.props.handleChange} cityValue={this.props.cityValue}></FormLocation>
-                <Citycard display_name={this.props.location}></Citycard>
+                {/* <Citycard display_name={this.props.location}></Citycard> */}
                 <DisplayModal error={this.props.error} hideError={this.props.hideError}></DisplayModal>
             </>
         )
